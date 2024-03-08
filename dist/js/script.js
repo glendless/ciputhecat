3fe5ac70f7d2341132b526968b40ce47cf8948a5
glendless <glenddelonge@gmail.com>
2024-03-09 05:49:02 +0700

    update dark mode + smooth scroll + img about section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,60 @@
 // navbar fixed
 window.onscroll = function () {
-  const header = document.querySelector("header");
-  const fixedNav = header.offsetTop;
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const backtoTop = document.querySelector('#back-to-top');
 
-  if (window.pageYOffset > fixedNav) {
-    header.classList.add("navbar-fixed");
-  } else {
-    header.classList.remove("navbar-fixed");
-  }
+    // if (window.parent !== window.top) {
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        backtoTop.classList.remove('hidden');
+        backtoTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        backtoTop.classList.remove('flex');
+        backtoTop.classList.add('hidden');
+    }
 };
 
 // hamburger menu
-const hamburger = document.querySelector("#hamburger");
+const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle('hidden');
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
 });
+
+window.addEventListener('click', function (e) {
+    if (e.target != navMenu && e.target != hamburger) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+const darkMode = document.querySelector('#dark-mode');
+const html = document.querySelector('html');
+
+darkMode.addEventListener('click', function () {
+    // darkMode.checked ? html.classList.add('dark') : html.classList.remove('dark');
+    if (darkMode.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// darkMode toggle
+if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+    // document.documentElement.classList.add('dark')
+    darkMode.checked = true;
+} else {
+    // document.documentElement.classList.remove('dark')
+    darkMode.checked = false;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
-  theme: {
-    container: {
-      center: true,
-      padding: '16px',
+    content: ['index.html'],
+    darkMode: 'class',
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            colors: {
+                // primary: '#ec4899',
+                primary: '#FF4081',
+                secondary: '#ff79b0',
+                dark: '#292929',
+                light: '#515151',
+                // accent: '#ff79b0',
+            },
+            screens: {
+                '2xl': '1320px',
+            },
+        },
     },
-    extend: {
-      colors: {
-        // primary: '#8b5cf6',
-        primary: '#ec4899',
-        secondary: '#f9a8d4',
-        dark: '#111827',
-        light: '#4b5563',
-      },
-      screens: {
-        '2xl': '1320px',
-      },
-    },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
